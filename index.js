3a2c9a285ecfbb54ea7939aa90909b9d7fe6947e
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-11 18:02:25 +0530

    read_from_console

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello I am learning Nodejs');
+const fs = require('fs');
+
+input = process.argv
+
+
+if(input[2] == 'add'){
+    fs.writeFileSync(input[3], input[4]);
+}
+else if(input[2] == 'remove'){
+    fs.unlinkSync(input[3]);
+}
+else{
+    console.log("Invalid Input")
+}
