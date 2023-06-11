a3e2302293b9da8a7c245f1a12f6176f2180e77a
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-11 18:31:45 +0530

    code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello I am learning Nodejs');
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
+console.warn(dirPath);
+
+// ***Create Single File***
+
+// fs.writeFileSync('apple.txt', 'This is apple file.')
+
+// ***Create multiple files using loop***
+
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(`${dirPath}/hello${i}.txt`, `This is hello${i} file.`);
+// }
+
+fs.readdir(dirPath, (err, files)=>{
+    files.forEach((item)=>{
+        console.log("file name is: ", item);
+    })
+})
