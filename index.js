cb53545a231d946e81fb153518486206856d09a9
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-12 08:45:25 +0530

    code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello I am learning Nodejs');
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname,'crud');
+const filePath = `${dirPath}/apple.txt` 
+
+// fs.writeFileSync(filePath, 'This is a simple text file.');
+
+// **** Append Data to File ****
+// fs.appendFile(filePath, 'and file name is apple.txt', (err)=>{
+//     if(!err) console.log("file is updated");
+// }) 
+
+// **** Update File name ****
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err)=>{
+//     if(!err) console.log("file name is updated")
+// })
+
+fs.unlinkSync(`${dirPath}/fruit.txt`);
+
