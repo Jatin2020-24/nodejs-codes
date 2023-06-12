317eda21dcf7bf822e70bd132f202d56359b7b68
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-12 15:37:27 +0530

    code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello I am learning Nodejs');
+const express = require('express');
+const app = express();
+
+app.get('', (req, res)=>{
+    console.log(`Data sent by browser: ${req.query.name}`)
+    res.send('<h1>Hello !! This is Jatin.inc</h1><a href="/about">Go to About Page</a>');
+});
+
+app.get('/about', (req, res)=>{
+    res.send(`
+        <input type="text" placeholder="Username" value="${req.query.name}" />
+        <button>Click Me</button>
+    `)
+});
+
+app.get('/help', (req, res)=>{
+    res.send({
+        name: 'Jatin',
+        email: 'jatinalwar2001@gmail.com'
+    })
+})
+
+app.listen(3000);
