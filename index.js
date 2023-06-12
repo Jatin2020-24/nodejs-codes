fa7f8e9c976e452b9df906631282a5629a3f417a
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-12 17:07:35 +0530

    code

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, 'public');
 
+
 app.use(express.static(publicPath));
+
+app.set('view engine', 'ejs');
 
 app.get('', (_, res)=>{
     res.sendFile(`${publicPath}/index.html`);
 });
 
-app.get('/about', (_, res)=>{
-    res.sendFile(`${publicPath}/about.html`);
+app.get('/profile', (_, res)=>{
+    const user = {
+        name: 'Jatin',
+        email: 'jatinalwar2001@gmail.com',
+        city: 'Alwar',
+        skills: ['python', 'c++', 'c']
+    }
+    res.render('profile', {user})
+});
+
+app.get('/login', (_, res)=>{
+    res.render('login');
 });
 
 app.get('/help', (_, res)=>{
