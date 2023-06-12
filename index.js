9c6530a62e4212da61227d9a9907faf58c33ec97
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-12 15:17:06 +0530

    code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello I am learning Nodejs');
+console.log("starting up");
+
+setTimeout(()=>{
+    console.log("2 second loop")
+}, 2000)
+
+setTimeout(()=>{
+    console.log("0 second loop")
+}, 0)
+
+console.log("finishing up")
