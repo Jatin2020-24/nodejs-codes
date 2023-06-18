ac569069e4b77c3871a25353533e866be06e83d7
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-19 00:15:34 +0530

    code

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const http = require('http');
 const express = require('express');
+const reqFilter = require('./middleware');
 const app = express();
 
-const reqFilter = (req, res, next)=>{
-    console.log("Filter Activated");
-    next();
-};
+const route = express.Router();
 
-app.use(reqFilter);
+// app.use(reqFilter);
 
-app.get('', (req, res)=>{
+route.use(reqFilter);
+
+app.get('', reqFilter, (req, res)=>{
     res.send('Welcome to Home Page');
 });
 
-app.get('/users', (req, res)=>{
+app.get('/users', reqFilter, (req, res)=>{
     res.send('Users Page');
 });
+
+route.get('/about', (req, res)=>{
+    res.send('About Page');
+});
+
+route.get('/contact', (req, res)=>{
+    res.send('For any query. Contact us !!');
+});
+
+app.use('/', route);
 
 app.listen(3000);
