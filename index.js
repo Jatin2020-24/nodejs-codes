a8739e2658bc6b7df9cb0127a54f999dd6ddec34
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2023-06-18 23:47:06 +0530

    code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello I am learning Nodejs');
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const reqFilter = (req, res, next)=>{
+    console.log("Filter Activated");
+    next();
+};
+
+app.use(reqFilter);
+
+app.get('', (req, res)=>{
+    res.send('Welcome to Home Page');
+});
+
+app.get('/users', (req, res)=>{
+    res.send('Users Page');
+});
+
+app.listen(3000);
